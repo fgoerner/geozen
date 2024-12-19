d251853415df070b5724a5048953e2345dccd158
Felix Görner <felix@goerner.dev>
2024-12-19 19:40:54 +0100

    Begin setting up build.gradle.kts for publishing to Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
-    id("java")
+    id("java-library")
+    id("maven-publish")
+    id("signing")
 }
 
 group = "dev.goerner.geozen"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -16,4 +18,55 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            pom {
+                name = "GeoZen"
+                description = "A Java library for working with geospatial data."
+                url = "https://github.com/fgoerner/geozen"
+
+                licenses {
+                    license {
+                        name = "MIT License"
+                        url = "https://github.com/fgoerner/geozen/blob/main/LICENSE"
+                    }
+                }
+
+                scm {
+                    connection = "scm:git:git://github.com/fgoerner/geozen.git"
+                    developerConnection = "scm:git:ssh://github.com/fgoerner/geozen.git"
+                    url = "https://github.com/fgoerner/geozen"
+                }
+
+                developers {
+                    developer {
+                        id = "fgoerner"
+                        name = "Felix Görner"
+                        email = "felix@goerner.dev"
+                    }
+                }
+            }
+        }
+    }
+}
+
+signing {
+    useInMemoryPgpKeys(
+        findProperty("signingKey") as String?,
+        findProperty("signingPassword") as String?
+    )
+    sign(publishing.publications["mavenJava"])
+}
+
+tasks.withType<Javadoc> {
+    (options as StandardJavadocDocletOptions).addStringOption("Xdoclint:none", "-quiet")
 }
