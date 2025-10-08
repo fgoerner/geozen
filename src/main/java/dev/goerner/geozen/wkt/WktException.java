6b13713113363677ad5cfd7f6b7b071ca09b9dbe
Felix Görner <felix@goerner.dev>
2025-10-08 23:54:58 +0200

    Implement WKT and EWKT deserialization and serialization for geometry types

---
+++ src/main/java/dev/goerner/geozen/wkt/WktException.java
@@ -0,0 +1,16 @@
+package dev.goerner.geozen.wkt;
+
+/**
+ * Exception thrown when an error occurs during WKT/EWKT serialization or deserialization.
+ */
+public class WktException extends RuntimeException {
+
+	public WktException(String message) {
+		super(message);
+	}
+
+	public WktException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
+
