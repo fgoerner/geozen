62603a446707af7f8592b3a262db360617333140
Felix Görner <felix@goerner.dev>
2026-02-07 21:56:26 +0100

    Update Position parameter documentation to reflect CoordinateReferenceSystem units

--- src/main/kotlin/dev/goerner/geozen/model/Position.kt
+++ src/main/kotlin/dev/goerner/geozen/model/Position.kt
@@ -3,10 +3,10 @@ package dev.goerner.geozen.model
 /**
  * A [Position] represents a point in space. It is defined by a longitude, latitude, and altitude.
  *
- * @param longitude The longitude in degrees, must be in the range [-180, 180]
- * @param latitude The latitude in degrees, must be in the range [-90, 90]
+ * @param longitude The longitude in the unit of the [CoordinateReferenceSystem] of its containing geometry
+ * @param latitude The latitude in the unit of the [CoordinateReferenceSystem] of its containing geometry
  * @param altitude The altitude in meters, defaults to 0.0
- * @throws IllegalArgumentException if longitude, latitude, or altitude are not finite or out of valid range
+ * @throws IllegalArgumentException if longitude, latitude, or altitude are not finite
  */
 data class Position(val longitude: Double, val latitude: Double, val altitude: Double = 0.0) {
     init {
